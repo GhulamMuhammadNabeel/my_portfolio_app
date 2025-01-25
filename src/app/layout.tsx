982738bc56658a5482f9ce0180ff4173504c126e
Ghulam Muhammad Nabeel <apnaqureshi5555@gmail.com>
2025-01-25 22:25:28 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const calistoga = Calistoga({
 });
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  description: "Created with the help of Allah Almighty",
 };
 
 export default function RootLayout({
