cdef76aae0ef3d8288f3709d3060ff1b68a5ed8f
Ghulam Muhammad Nabeel <apnaqureshi5555@gmail.com>
2025-09-29 08:21:54 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
+// app/page.tsx
+import { Metadata } from "next";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
-import {ProjectsSection} from '@/sections/Projects';
-import {TapeSection} from '@/sections/Tape';
-import {TestimonialsSection} from '@/sections/Testimonials'
-import {AboutSection} from '@/sections/About'
-import {ContactSection} from '@/sections/Contact'
-import {Footer} from '@/sections/Footer'
+import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
+import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
+
+export const metadata: Metadata = {
+  title: "Home | Ghulam Muhammad Nabeel",
+  description: "Welcome to the portfolio of Ghulam Muhammad Nabeel - showcasing top projects, testimonials, and expertise.",
+};
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <HeroSection/>
-      <ProjectsSection/>
-      <TapeSection/>
-      <TestimonialsSection/>
-      <AboutSection/>
-      <ContactSection/>
-      <Footer/>
+      <Header />
+      <HeroSection />
+      <ProjectsSection />
+      <TapeSection />
+      <TestimonialsSection />
+      <AboutSection />
+      <ContactSection />
+      <Footer />
     </div>
   );
 }
-
